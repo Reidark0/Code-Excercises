13998cd5014816fa92a0bdec5f8c30cb78392e8d
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-05-11 15:21:54 -0300

    edit: resilia aula3-2

--- Resilia/python/Modulo 2/Aula3-2.py
+++ Resilia/python/Modulo 2/Aula3-2.py
@@ -1,12 +1,9 @@
 # Retirar os numeros impar e ordenar do menor pro maior
 lista = [1, 3, 76, 12, 45, 864, 98, 23, 87, 24, 80, 456, 152, 43, 900]
+listaFinal = []
+for i in range(len(lista)):
+    if lista[i] % 2 == 0:
+        listaFinal.append(lista[i])
 
-while lista:
-    for i in range(len(lista)):
-        print(lista)
-        if lista[i] % 2 == 1:
-            print('deletar', lista[i])
-            lista.pop(i)
-            break
-
-print(lista)
+listaFinal.sort()
+print(listaFinal)
