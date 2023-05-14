d2c03d50a10a92a0b5bcadb333fc398ed868e196
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-05-13 22:30:35 -0300

    feature: add Resilia Fila-de-espera

---
+++ Resilia/python/Modulo 2/Fila-de-espera.py
@@ -0,0 +1,12 @@
+# Crie uma fila de espera com os nomes de cinco pessoas e execute as seguintes operações: 
+# 1-Imprima na tela a fila atual. 
+# 2-Remova o primeiro elemento da fila e imprima na tela a fila atualizada. 
+# 3-Adicione mais dois nomes à fila e imprima na tela a fila atualizada.
+
+fila = ['Rafael', 'Will', 'Rafa', 'Vic', 'Esli']
+print(fila)
+fila.pop(0)
+print(fila)
+fila.append('Luciana')
+fila.append('Daniel')
+print(fila)
