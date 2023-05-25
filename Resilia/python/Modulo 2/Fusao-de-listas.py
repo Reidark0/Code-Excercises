70207d7fd1ed87d9e100390d23fef03b91eded7d
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-05-24 22:03:10 -0300

    add: exercicios da resilia do modulo 2

--- Resilia/python/Modulo 2/Fusao-de-listas.py
+++ Resilia/python/Modulo 2/Fusao-de-listas.py
@@ -1,10 +1,6 @@
 lista1 = [1, 2, 3, 4, 5]
 lista2 = [6, 7, 8, 9, 0]
-lista3 = [numeros for numeros in range(len(lista1)) for numeros in [lista1[numeros], lista2[numeros]]]
-print(lista3)
-lista3 = []
-for i in range(len(lista2)):
-    lista3.append(lista1[i])
-    lista3.append(lista2[i])
+
+lista3 = [x for i in range(len(lista2)) for x in [lista1[i], lista2[i]]]
 
 print(lista3)
---
+++ Resilia/python/Modulo 2/Soma-de-valores-em-uma-lista.py
@@ -0,0 +1,9 @@
+lista = [1, 2, 3, 4, 5]
+soma = 0
+
+for i in lista:
+    soma += i
+
+print(soma)
+
+print(sum(lista))
