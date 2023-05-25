c804d6eb3055ffea3195922b594cadd9fe77ab2f
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-05-25 11:42:18 -0300

    add: resilia exercicio5

---
+++ Resilia/python/Modulo 2/exercicio5.py
@@ -0,0 +1,12 @@
+# Fatiando listas
+# 
+# Crie uma lista com 6 elementos e utilize o fatiamento 
+# para imprimir na tela as seguintes sub-listas: 
+# 1 - Os três primeiros elementos
+# 2 - Os três últimos elementos
+# 3 - Sub-lista com o segundo e o terceiro elemento.
+
+lista = [1, 2, 3, 4, 5, 6]
+print(lista[:3])
+print(lista[3:])
+print(lista[1:3])
