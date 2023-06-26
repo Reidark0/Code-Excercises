5f3de9baad44fef6fffc53674ee17c14bc0e7f50
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-06-26 15:21:39 -0300

    add Resilia Aula 7

---
+++ Resilia/Modulo 3/sql/Aula7-1.sql
@@ -0,0 +1,11 @@
+SELECT nome AS Clientes FROM tb_cliente;
+
+SELECT nome AS Pizzas FROM tb_pizza;
+
+SELECT nome AS Pizzas FROM tb_pizza WHERE categoria = 'Zero Lactose';
+
+SELECT nome AS Pizzas FROM tb_pizza WHERE NOT categoria = 'Zero Lactose';
+
+SELECT * FROM tb_pedido;
+
+SELECT * FROM pizza;
